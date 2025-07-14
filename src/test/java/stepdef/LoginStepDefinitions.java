09e979b9aac28aa5277999081b3d37f84942d31d
sbonilla <sayubonillagarcia@gmail.com>
2025-07-14 15:13:35 -0500

    cambios en clase stepdef

--- src/test/java/stepdef/LoginStepDefinitions.java
+++ src/test/java/stepdef/LoginStepDefinitions.java
@@ -4,34 +4,47 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import net.serenitybdd.annotations.Managed;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
+import org.openqa.selenium.WebDriver;
 import questions.LoginResult;
 import tasks.Login;
 
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static org.hamcrest.Matchers.equalTo;
+
+git
 public class LoginStepDefinitions {
 
-    @Before
-    public void  setUp(){OnStage.setTheStage(new OnlineCast()); }
+        @Managed(driver = "chrome")
+        WebDriver driver;
 
-    @Given("the user opens the login page")
-        public void theUserOpensTheLoginPage(){
-        OnStage.theActorCalled("User").wasAbleTo();
+        @Before
+        public void setUp() {
+            OnStage.setTheStage(new OnlineCast());
+            OnStage.theActorCalled("User").can(BrowseTheWeb.with(driver));
+        }
 
+        @Given("the user opens the login page")
+        public void theUserOpensTheLoginPage() {
+            // Aquí puedes abrir directamente la URL si aún no usas una tarea "OpenPage"
+            driver.get("https://releaseqa3.arandasoft.com/adm/Pages/app/login");
+        }
+
+        @When("they enter valid credentials")
+        public void theyEnterValidcredentials() {
+            OnStage.theActorInTheSpotlight().attemptsTo(
+                    Login.withCredentials("admin", "admin123")
+            );
+        }
+
+        @Then("they should be redirected to the dashboard")
+        public void theyShouldBeRedirectedToTheDashboard() {
+            OnStage.theActorInTheSpotlight().should(
+                    seeThat(LoginResult.message(), equalTo("Welcome!"))
+            );
+        }
     }
 
-    @When("they enter valid credentials")
-        public void theyEnterValidcredentials(){
-        OnStage.theActorInTheSpotlight().attemptsTo(Login.withCredentials("admin", "admin123"));
-    }
-
-    @Then("they should be redirected to the dashboard")
-    public void theyShouldBeRedirectedToTheDashboard() {
-        OnStage.theActorInTheSpotlight().should(
-                seeThat(LoginResult.message(), equalTo("Welcome!"))
-        );
-    }
-
-}
